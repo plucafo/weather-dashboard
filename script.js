8e43c6401ce628993b6ee9a3e2e4d76feaf56cd5
plucafo <plucaf87@gmail.com>
2024-01-29 15:26:40 -0700

    created function to get and display the weather data so it can be used to display a default location on page load as well as update when a user submits a new city to the form

--- script.js
+++ script.js
@@ -18,68 +18,34 @@ for (var i = 0; i < cities.length; i++) {
   buttonEl.append(cityBtn);
 }
 
-// Maybe make an array of the lat and lon for each city?
-// var lat = ['41.88', ];
-// var lon = ['87.62', ];
-
 // Element variables
 var weatherEl = $(".weather-info");
 var cityNameEl = $("<h3>");
 var conditionEl = $("<h6>");
 var iconEl = $("<img>");
 var cityTempEl = $("<h6>");
-
 var searchForm = $(".search-form");
-var userSelectedCity;
-var weatherURL;
 
-searchForm.on('submit', function(event){
+// Sets Chicago as default weather displayed on page load
+var userSelectedCity = "Chicago"; // Set this to any city to change default weather shown
+var weatherURL = `https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${userSelectedCity}&appid=976a6e1bd50b752c93e255a6e65ac032`;
+getWeatherData();
+
+// Event listener on form submit
+searchForm.on("submit", function (event) {
   event.preventDefault();
 
-  userSelectedCity = searchForm.children('input').val();
+  userSelectedCity = searchForm.children("input").val();
 
   weatherURL = `https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${userSelectedCity}&appid=976a6e1bd50b752c93e255a6e65ac032`;
 
-  console.log('Submitted City: ' + userSelectedCity);
+  console.log("Submitted City: " + userSelectedCity); // Testing
 
-  fetch(weatherURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (weatherData) {
-    console.log(weatherData);
+  getWeatherData();
+});
 
-    var temp = weatherData.main.temp;
-    var condition = weatherData.weather[0].description;
-    var location = weatherData.name;
-    var icon = weatherData.weather[0].icon;
-
-    console.log(
-      `Temperature in ${location}: ${temp}°F
-      Weather Condition: ${condition}
-      Icon: ${icon}`
-    );
-
-    // Add content to elements
-    cityNameEl.text(location);
-    cityTempEl.text(`${temp}°F`);
-    conditionEl.text(condition);
-    iconEl.attr("src", "https://openweathermap.org/img/wn/" + icon + "@2x.png");
-    // weatherEl.addClass('d-flex align-items-center')
-    // Append elements to the weather info field
-    weatherEl.append(iconEl);
-    weatherEl.append(cityNameEl);
-    weatherEl.append(conditionEl);
-    weatherEl.append(cityTempEl);
-  });
-})
-
-// Fetch URLs
-// var weatherURL =
-//   "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=chicago&appid=976a6e1bd50b752c93e255a6e65ac032";
 var forecastURL =
   "https://api.openweathermap.org/data/2.5/forecast?units=imperial&q=chicago&appid=976a6e1bd50b752c93e255a6e65ac032";
-
 
 // Fetch forecast data
 fetch(forecastURL)
@@ -89,4 +55,43 @@ fetch(forecastURL)
   .then(function (forecastData) {
     console.log(forecastData);
   });
-  
+
+// **************************************************************************
+// ********************* Get weather data function **************************
+// **** Gets API data from open weather and appends the info to the page ****
+// **************************************************************************
+function getWeatherData() {
+  fetch(weatherURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (weatherData) {
+      console.log(weatherData);
+
+      var temp = weatherData.main.temp;
+      var condition = weatherData.weather[0].description;
+      var location = weatherData.name;
+      var icon = weatherData.weather[0].icon;
+
+      console.log(
+        `Temperature in ${location}: ${temp}°F
+      Weather Condition: ${condition}
+      Icon: ${icon}`
+      );
+
+      // Add content to elements
+      cityNameEl.text(location);
+      cityTempEl.text(`${temp}°F`);
+      conditionEl.text(condition);
+      iconEl.attr(
+        "src",
+        "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+      );
+
+      // Append elements to the weather info field
+      weatherEl.append(iconEl);
+      weatherEl.append(cityNameEl);
+      weatherEl.append(conditionEl);
+      weatherEl.append(cityTempEl);
+    });
+}
