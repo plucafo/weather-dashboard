35d73bcaef53fe4e66703977b8c57ba8505baa1b
plucafo <plucaf87@gmail.com>
2024-01-31 13:04:15 -0700

    added variables for forecast API data

--- script.js
+++ script.js
@@ -18,83 +18,40 @@ var cities = [
   "Phoenix",
 ];
 
-// Creates buttons using cities array
+// Sets Chicago as default weather displayed on page load
+var userSelectedCity = "Chicago"; // Set this to any city to change default weather shown
+weatherURL = `https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${userSelectedCity}&appid=976a6e1bd50b752c93e255a6e65ac032`;
+
+getWeatherData();
+
+// Fetch forecast data
+var forecastURL =
+  "https://api.openweathermap.org/data/2.5/forecast?q=Chicago&units=imperial&appid=976a6e1bd50b752c93e255a6e65ac032";
+
+fetch(forecastURL)
+  .then(function (response2) {
+    return response2.json();
+  })
+  .then(function (forecastData) {
+    console.log("forecastData: ");
+    console.log(forecastData);
+    
+    var forecastDate = forecastData.list[i].dt_txt.split(" ")[0];
+    var forecastIcon = forecastData.list[0].weather[0].icon;
+    var forecastTemp = forecastData.list[i].main.temp;
+    var forecastWind = forecastData.list[i].wind.speed;
+    var forecastHumidity = forecastData.list[i].main.humidity;
+    // Gets time of day for each 3-hour forecast index
+    var timeStamp = forecastData.list[0].dt_txt.split(" ")[1];
+    console.log(timeStamp);
+
+  });
+
 for (var i = 0; i < cities.length; i++) {
+  // Create button elements using names from cities array
   var cityBtn = $("<button>")
     .text(cities[i])
     .addClass("btn bg-secondary-subtle text-dark mb-3 w-100");
-
-  // Sets Chicago as default weather displayed on page load
-  var userSelectedCity = "Chicago"; // Set this to any city to change default weather shown
-  weatherURL = `https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${userSelectedCity}&appid=976a6e1bd50b752c93e255a6e65ac032`;
-  getWeatherData();
-
-  // Fetch forecast data
-
-  var forecastURL =
-    "https://api.openweathermap.org/data/2.5/forecast?q=Chicago&units=imperial&appid=976a6e1bd50b752c93e255a6e65ac032";
-
-  fetch(forecastURL)
-    .then(function (response2) {
-      return response2.json();
-    })
-    .then(function (forecastData) {
-      console.log("forecastData: ");
-      console.log(forecastData);
-    });
-
-  // **************************************************************************
-  // ********************* Get weather data function **************************
-  // **** Gets API data from open weather and appends the info to the page ****
-  // **************************************************************************
-  function getWeatherData() {
-    fetch(weatherURL)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (weatherData) {
-        console.log("weatherData: ");
-        console.log(weatherData);
-
-        var temp = weatherData.main.temp;
-        var condition = weatherData.weather[0].description;
-        var location = weatherData.name;
-        var icon = weatherData.weather[0].icon;
-
-        console.log(
-          `Temperature in ${location}: ${temp}°F
-      Weather Condition: ${condition}
-      Icon: ${icon}`
-        );
-
-        // Add content to elements
-        cityNameEl.text(location);
-        cityTempEl.text(`${temp}°F`);
-        conditionEl.text(condition);
-        iconEl.attr(
-          "src",
-          "https://openweathermap.org/img/wn/" + icon + "@2x.png"
-        );
-
-        // Append elements to the weather info field
-        weatherEl.append(iconEl);
-        weatherEl.append(cityNameEl);
-        weatherEl.append(conditionEl);
-        weatherEl.append(cityTempEl);
-      });
-  }
-
-  // Handle form submit function
-  function handleFormSubmit(event) {
-    event.preventDefault();
-    userSelectedCity = searchForm.children("input").val();
-    weatherURL = `https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${userSelectedCity}&appid=976a6e1bd50b752c93e255a6e65ac032`;
-
-    getWeatherData();
-  }
-
-  // Event listener on form submit
-  searchForm.on("submit", handleFormSubmit);
 
   // Event listener for side buttons
   cityBtn.on("click", function () {
@@ -105,3 +62,56 @@ for (var i = 0; i < cities.length; i++) {
 
   buttonEl.append(cityBtn);
 }
+
+// **************************************************************************
+// ********************* Get weather data function **************************
+// **** Gets API data from open weather and appends the info to the page ****
+// **************************************************************************
+function getWeatherData() {
+  fetch(weatherURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (weatherData) {
+      console.log("weatherData: ");
+      console.log(weatherData);
+
+      var temp = weatherData.main.temp;
+      var condition = weatherData.weather[0].description;
+      var location = weatherData.name;
+      var icon = weatherData.weather[0].icon;
+
+      console.log(
+        `Temperature in ${location}: ${temp}°F
+      Weather Condition: ${condition}
+      Icon: ${icon}`
+      );
+
+      // Add content to elements
+      cityNameEl.text(location);
+      cityTempEl.text(`${temp}°F`);
+      conditionEl.text(condition);
+      iconEl.attr(
+        "src",
+        "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+      );
+
+      // Append elements to the weather info field
+      weatherEl.append(iconEl);
+      weatherEl.append(cityNameEl);
+      weatherEl.append(conditionEl);
+      weatherEl.append(cityTempEl);
+    });
+}
+
+// Handles form submission
+function handleFormSubmit(event) {
+  event.preventDefault();
+  userSelectedCity = searchForm.children("input").val();
+  weatherURL = `https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${userSelectedCity}&appid=976a6e1bd50b752c93e255a6e65ac032`;
+
+  getWeatherData();
+}
+
+// Event listener on form submit
+searchForm.on("submit", handleFormSubmit);
