1d2953eb4b8a0b754eeda3974e971d542291822a
plucafo <plucaf87@gmail.com>
2024-02-01 09:11:10 -0700

    added conditional so a button is only created if the API data is returned succesful with a status of 200. Also created function to capitalize the first letter of the city that the user submits

--- script.js
+++ script.js
@@ -12,124 +12,39 @@ var buttonEl = $(".city-buttons");
 var weatherURL;
 var forecastURL;
 
-var cities = [];
+var cities = []; // Create empty array to add user submitted city names to
 
 // Sets Chicago as default weather displayed on page load
 var userSelectedCity = "Chicago"; // Set this to any city to change default weather shown
 weatherURL = `https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${userSelectedCity}&appid=976a6e1bd50b752c93e255a6e65ac032`;
-forecastURL =
-  `https://api.openweathermap.org/data/2.5/forecast?q=${userSelectedCity}&units=imperial&appid=976a6e1bd50b752c93e255a6e65ac032`;
+forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${userSelectedCity}&units=imperial&appid=976a6e1bd50b752c93e255a6e65ac032`;
 
 getWeatherData();
 getForecastData();
 
-// Fetch forecast data
-function getForecastData() {
-fetch(forecastURL)
-  .then(function (response2) {
-    return response2.json();
-  })
-  .then(function (forecastData) {
-    console.log("forecastData: ");
-    console.log(forecastData);
-    
-    
-    for (var i=0; i < 40; i++) {
-      // Variables to collect API data
-      var forecastDate = forecastData.list[i].dt_txt.split(" ")[0];
-      var forecastIcon = forecastData.list[i].weather[0].icon;
-      var forecastTemp = forecastData.list[i].main.feels_like;
-      var forecastWind = forecastData.list[i].wind.speed;
-      var forecastHumidity = forecastData.list[i].main.humidity;
-      var forecastCity = forecastData.city.name;
-      var timeStamp = forecastData.list[i].dt_txt.split(" ")[1];
-
-      // Varibles to create card elements
-      var forecastCardEl = $('<div>').addClass('card text-bg-primary my-3 p-3 custom-height');
-      var forecastDateEl = $('<h5>').text(forecastDate);
-      var forecastIconEl = $('<img>').attr('src', 'https://openweathermap.org/img/wn/' + forecastIcon + '@2x.png').addClass('w-25');
-      var hrEl = $('<hr>');
-      var forecastCityEl = $('<p>').text(forecastCity);
-      var forecastTempEl = $('<p>').text(`Temp: ${forecastTemp}°F`);
-      var forecastWindEl = $('<p>').text(`Wind: ${forecastWind}MPH`);
-      var forecastHumidityEl = $('<p>').text(`Humidity: ${forecastHumidity}%`);
-      
-      // Append elements with data to the forecastCardEl if times match
-      if(timeStamp == '00:00:00') {
-        forecastEl.append(forecastCardEl);
-        forecastCardEl.append(forecastDateEl);
-        forecastCardEl.append(forecastIconEl);
-        forecastCardEl.append(hrEl);
-        forecastCardEl.append(forecastCityEl);
-        forecastCardEl.append(forecastTempEl);
-        forecastCardEl.append(forecastWindEl);
-        forecastCardEl.append(forecastHumidityEl);
-      }
-    }
-
-  });
-}
-
-// function createButton () {
-// if (buttonEl.children.length > 6) {
-//   buttonEl.lastChildElement.remove();
-// } else {
-// var cityBtn = $("<button>")
-//     .text('button')
-//     .addClass("btn bg-secondary-subtle text-dark mb-3 w-100");
-
-//     buttonEl.append(cityBtn);
-// }
-// }
-
-// Create button elements using names from cities array
-function createButton () {
-  buttonEl.empty();
-  cities = cities.slice(0, 6);
-
-for (var i = 0; i < cities.length; i++) {
-  var cityBtn = $("<button>")
-    .text(cities[i])
-    .addClass("btn bg-secondary-subtle text-dark mb-3 w-100");
-
-  // Event listener for side buttons
-  cityBtn.on("click", function () {
-    forecastEl.empty();
-    var clickedBtn = $(this).text();
-    weatherURL = `https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${clickedBtn}&appid=976a6e1bd50b752c93e255a6e65ac032`;
-   forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${clickedBtn}&units=imperial&appid=976a6e1bd50b752c93e255a6e65ac032`;
-
-    getWeatherData();
-    getForecastData();
-  });
-
-  buttonEl.append(cityBtn);
-}
-}
-
-// **************************************************************************
-// ********************* Get weather data function **************************
-// **** Gets API data from open weather and appends the info to the page ****
-// **************************************************************************
+// Gets API data from open weather and appends the info to the page
 function getWeatherData() {
   fetch(weatherURL)
     .then(function (response) {
+      if (response.status !== 200) {
+        // Don't create button if API call is unsuccessful
+        return;
+      }
       return response.json();
     })
     .then(function (weatherData) {
       console.log("weatherData: ");
       console.log(weatherData);
 
+      if (weatherData) {
+        // Only create button if weatherData is available
+        createButton(userSelectedCity);
+      }
+
       var temp = weatherData.main.temp;
       var condition = weatherData.weather[0].description;
       var location = weatherData.name;
       var icon = weatherData.weather[0].icon;
-
-      console.log(
-        `Temperature in ${location}: ${temp}°F
-      Weather Condition: ${condition}
-      Icon: ${icon}`
-      );
 
       // Add content to elements
       cityNameEl.text(location);
@@ -148,19 +63,113 @@ function getWeatherData() {
     });
 }
 
+// Fetch forecast data
+function getForecastData() {
+  fetch(forecastURL)
+    .then(function (response2) {
+      if (response2.status !== 200) {
+        // Don't create button if API call is unsuccessful
+        return;
+      }
+      return response2.json();
+    })
+    .then(function (forecastData) {
+      console.log("forecastData: ");
+      console.log(forecastData);
+
+      if (forecastData) {
+        // Only create button if weatherData is available
+        createButton(userSelectedCity);
+      }
+
+      for (var i = 0; i < 40; i++) {
+        // Variables to collect API data
+        var forecastDate = forecastData.list[i].dt_txt.split(" ")[0];
+        var forecastIcon = forecastData.list[i].weather[0].icon;
+        var forecastTemp = forecastData.list[i].main.feels_like;
+        var forecastWind = forecastData.list[i].wind.speed;
+        var forecastHumidity = forecastData.list[i].main.humidity;
+        var forecastCity = forecastData.city.name;
+        var timeStamp = forecastData.list[i].dt_txt.split(" ")[1];
+
+        // Varibles to create card elements
+        var forecastCardEl = $("<div>").addClass(
+          "card text-bg-primary my-3 p-3 custom-height"
+        );
+        var forecastDateEl = $("<h5>").text(forecastDate);
+        var forecastIconEl = $("<img>")
+          .attr(
+            "src",
+            "https://openweathermap.org/img/wn/" + forecastIcon + "@2x.png"
+          )
+          .addClass("w-25");
+        var hrEl = $("<hr>");
+        var forecastCityEl = $("<p>").text(forecastCity);
+        var forecastTempEl = $("<p>").text(`Temp: ${forecastTemp}°F`);
+        var forecastWindEl = $("<p>").text(`Wind: ${forecastWind}MPH`);
+        var forecastHumidityEl = $("<p>").text(
+          `Humidity: ${forecastHumidity}%`
+        );
+
+        // Append elements with data to the forecastCardEl if times match
+        if (timeStamp == "00:00:00") {
+          forecastEl.append(forecastCardEl);
+          forecastCardEl.append(forecastDateEl);
+          forecastCardEl.append(forecastIconEl);
+          forecastCardEl.append(hrEl);
+          forecastCardEl.append(forecastCityEl);
+          forecastCardEl.append(forecastTempEl);
+          forecastCardEl.append(forecastWindEl);
+          forecastCardEl.append(forecastHumidityEl);
+        }
+      }
+    });
+}
+
+// Create button elements using names from cities array
+function createButton() {
+  buttonEl.empty();
+  cities = cities.slice(0, 6);
+
+  for (var i = 0; i < cities.length; i++) {
+    var cityBtn = $("<button>")
+      .text(cities[i])
+      .addClass("btn bg-secondary-subtle text-dark mb-3 w-100");
+
+    // Event listener for side buttons
+    cityBtn.on("click", function () {
+      forecastEl.empty();
+      var clickedBtn = $(this).text();
+      weatherURL = `https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${clickedBtn}&appid=976a6e1bd50b752c93e255a6e65ac032`;
+      forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${clickedBtn}&units=imperial&appid=976a6e1bd50b752c93e255a6e65ac032`;
+
+      getWeatherData();
+      getForecastData();
+    });
+
+    buttonEl.append(cityBtn);
+  }
+}
+
 // Handles form submission
 function handleFormSubmit(event) {
   event.preventDefault();
   forecastEl.empty();
-  userSelectedCity = searchForm.children("input").val().split(',')[0].trim();
+  userSelectedCity = capitalizeFirstLetter(searchForm.children("input").val().split(",")[0].trim());
   weatherURL = `https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${userSelectedCity}&appid=976a6e1bd50b752c93e255a6e65ac032`;
   forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${userSelectedCity}&units=imperial&appid=976a6e1bd50b752c93e255a6e65ac032`;
-  
-  cities.unshift(userSelectedCity);
 
-  getWeatherData();
-  getForecastData();
-  createButton();
+  fetch(weatherURL).then(function (response) {
+    if (response.status === 200) {
+      cities.unshift(userSelectedCity);
+      getWeatherData();
+      getForecastData();
+    }
+  });
+}
+
+function capitalizeFirstLetter(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
 // Event listener on form submit
